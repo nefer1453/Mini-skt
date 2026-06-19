18f6fdc2df19c15eeac7781ac95300b3dad4641b
nefer1453 <selcukkashna@gmail.com>
2026-06-19 19:17:58 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'skt-kontrol-v1';
+const ASSETS = [
+    '/Mini-skt/',
+    '/Mini-skt/index.html',
+    '/Mini-skt/style.css',
+    '/Mini-skt/app.js',
+    '/Mini-skt/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
